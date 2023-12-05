96c3264b79519459dc0e7c86b225ac081fab286e
Jonathan Sigrist <jonathan.sigrist@xitaso.com>
2023-12-06 00:34:46 +0100

    test case for adding items

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,3 +1,18 @@
-def test_example():
-    assert(1+1 == 2)
+def test__init_empty_manager__should_be_empty():
+    inventory = InventoryManager()
+    assert len(inventory) == 0
+
+def test__add_items__should_be_saved():
+    inventory = InventoryManager()
+    inventory.add("milk")
+    assert len(inventory) == 1
+    inventory.add("sugar")
+    assert len(inventory) == 2
+    assert set(inventory.items) == set(["milk", "sugar"])
+
+def test__add_items_twice__should_add_once():
+    inventory = InventoryManager()
+    inventory.add("milk")
+    inventory.add("milk")
+    assert len(inventory) == 1
     
