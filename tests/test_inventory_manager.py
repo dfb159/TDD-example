fe4b4fdf8326456b749689678ba856919ed96a42
Jonathan Sigrist <jonathan.sigrist@xitaso.com>
2023-12-06 01:35:55 +0100

    linting: add docstring

--- tests/test_inventory_manager.py
+++ tests/test_inventory_manager.py
@@ -1,12 +1,15 @@
+"""Tests for the basic Inventory API."""
 from inventory_app.inventory_manager import InventoryManager
 
 
-def test__init_empty_manager__should_be_empty():
+def test__init_empty_manager():
+    """A newly instanciated manager should be empty."""
     inventory = InventoryManager()
     assert len(inventory) == 0
 
 
-def test__add_items__should_be_saved():
+def test__add_items():
+    """Adding items to the manager should be persisted."""
     inventory = InventoryManager()
     inventory.add("milk")
     assert len(inventory) == 1
@@ -15,14 +18,16 @@ def test__add_items__should_be_saved():
     assert set(inventory.items()) == set(["milk", "sugar"])
 
 
-def test__add_items_twice__should_add_once():
+def test__add_items_twice():
+    """Adding the same item twice should still be added, but not throw an exception."""
     inventory = InventoryManager()
     inventory.add("milk")
     inventory.add("milk")
     assert len(inventory) == 1
 
 
-def test__remove_item_should_work():
+def test__remove_item():
+    """Removing an item will delete it from the list."""
     inventory = InventoryManager()
     inventory.add("milk")
     assert len(inventory) == 1
@@ -30,7 +35,8 @@ def test__remove_item_should_work():
     assert len(inventory) == 0
 
 
-def test__remove_item_then_add_should_work():
+def test__remove_item_then_add():
+    """Adding a removed item should not throw."""
     inventory = InventoryManager()
     inventory.add("milk")
     assert len(inventory) == 1
@@ -42,7 +48,8 @@ def test__remove_item_then_add_should_work():
     assert len(inventory) == 0
 
 
-def test__remove_item_twice_should_not_error():
+def test__remove_item_twice():
+    """Removing an item twice does not throw."""
     inventory = InventoryManager()
     inventory.add("milk")
     assert len(inventory) == 1
