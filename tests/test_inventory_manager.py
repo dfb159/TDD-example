ee6d32e9bfc3e5ccfbeab4a42d7f86c0ff2f7a07
feba66 <git@feba66.de>
2023-12-06 01:26:13 +0100

    tests for removing items

--- tests/test_inventory_manager.py
+++ tests/test_inventory_manager.py
@@ -5,6 +5,7 @@ def test__init_empty_manager__should_be_empty():
     inventory = InventoryManager()
     assert len(inventory) == 0
 
+
 def test__add_items__should_be_saved():
     inventory = InventoryManager()
     inventory.add("milk")
@@ -13,9 +14,38 @@ def test__add_items__should_be_saved():
     assert len(inventory) == 2
     assert set(inventory.items()) == set(["milk", "sugar"])
 
+
 def test__add_items_twice__should_add_once():
     inventory = InventoryManager()
     inventory.add("milk")
     inventory.add("milk")
     assert len(inventory) == 1
-    
+
+
+def test__remove_item_should_work():
+    inventory = InventoryManager()
+    inventory.add("milk")
+    assert len(inventory) == 1
+    inventory.remove("milk")
+    assert len(inventory) == 0
+
+
+def test__remove_item_then_add_should_work():
+    inventory = InventoryManager()
+    inventory.add("milk")
+    assert len(inventory) == 1
+    inventory.remove("milk")
+    assert len(inventory) == 0
+    inventory.add("milk")
+    assert len(inventory) == 1
+    inventory.remove("milk")
+    assert len(inventory) == 0
+
+
+def test__remove_item_twice_should_not_error():
+    inventory = InventoryManager()
+    inventory.add("milk")
+    assert len(inventory) == 1
+    inventory.remove("milk")
+    inventory.remove("milk")
+    assert len(inventory) == 0
